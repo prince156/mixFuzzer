ccf333c649c4a113962afc0e4af2abf5aff88715
littledj <littledj@local.com>
2016-04-21 10:38:17 +0800

    新建

---
+++ mixFuzzer/fuzzstr.h
@@ -0,0 +1,24 @@
+#pragma once
+
+vector<string> compatibleName = {
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=5\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=7\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=8\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=9\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=10\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=EmulateIE7\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=EmulateIE8\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=EmulateIE9\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=EmulateIE10\">",
+    "<meta http-equiv=\"x-ua-compatible\" content=\"IE=edge\">" 
+};
+
+vector<string> doctypeName = { 
+    "",
+    "<!DOCTYPE html>",
+    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">",
+    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">",
+    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">",
+    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">"
+};
+
